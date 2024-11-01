a602692519bd21d35abaf7fcdeadbb6af7698b60
Chris Dymond <dwarfered@users.noreply.github.com>
2024-11-01 19:24:46 +0800

    sample purge for 14.2.16

--- sample/components/ErrorComponent.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { MsalAuthenticationResult } from "@azure/msal-react";
-
-export const ErrorComponent = (error: MsalAuthenticationResult) => {
-    return <h6 >An Error Occurred: {error ? error.error?.errorCode : "unknown error"}</h6>;
-}
